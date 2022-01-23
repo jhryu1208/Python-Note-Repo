bfc0492150a7387015d7346ae5a5aa2dff01c6e1
jhryu1208 <jhryu1208@gmail.com>
2022-01-23 19:56:28 +0900

    add: blob metadata property

--- gcp/gcs/listup_blobs.py
+++ gcp/gcs/listup_blobs.py
@@ -16,7 +16,7 @@ class blob_test:
         storage_client = self.storage_client
         bucket_name = self.bucket_name
         """
-        list_blobs 
+        list_blobs
         return : iterator of all blobs in the bucket
         """
         blobs_iter = storage_client.list_blobs(bucket_name)
@@ -30,14 +30,45 @@ if __name__ == '__main__':
     b = blob_test(storage_client, BUCKET_NAME)
     blobs = b.list_blobs()
     for num, blob in enumerate(blobs, 1):
-        name = blob.name
-        type = blob.content_type
-        bucket = blob.bucket
-        size = blob.size
+
         print(f"""
         [ blob{num} ]
-        name : {name}
-        type : {type}
-        bucket : {bucket}
-        size : {size} B
+        name: {blob.name}
+        type: {blob.content_type}
+        bucket: {blob.bucket}
+        size: {blob.size} B
         """)
+
+"""
+    print("Blob: {}".format(blob.name))
+    print("Bucket: {}".format(blob.bucket.name))
+    print("Storage class: {}".format(blob.storage_class))
+    print("ID: {}".format(blob.id))
+    print("Size: {} bytes".format(blob.size))
+    print("Updated: {}".format(blob.updated))
+    print("Generation: {}".format(blob.generation))
+    print("Metageneration: {}".format(blob.metageneration))
+    print("Etag: {}".format(blob.etag))
+    print("Owner: {}".format(blob.owner))
+    print("Component count: {}".format(blob.component_count))
+    print("Crc32c: {}".format(blob.crc32c))
+    print("md5_hash: {}".format(blob.md5_hash))
+    print("Cache-control: {}".format(blob.cache_control))
+    print("Content-type: {}".format(blob.content_type))
+    print("Content-disposition: {}".format(blob.content_disposition))
+    print("Content-encoding: {}".format(blob.content_encoding))
+    print("Content-language: {}".format(blob.content_language))
+    print("Metadata: {}".format(blob.metadata))
+    print("Custom Time: {}".format(blob.custom_time))
+    print("Temporary hold: ", "enabled" if blob.temporary_hold else "disabled")
+    print(
+        "Event based hold: ",
+        "enabled" if blob.event_based_hold else "disabled",
+    )
+    if blob.retention_expiration_time:
+        print(
+            "retentionExpirationTime: {}".format(
+                blob.retention_expiration_time
+            )
+        )
+"""
